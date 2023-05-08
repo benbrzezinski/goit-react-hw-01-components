84a40b5eb0f9f7db3ffc8a537754757a5d84f681
Beniamin Brzeziński <benek9311@wp.pl>
2023-05-08 14:46:30 +0200

    Added profile component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,14 @@
-const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import user from '../json/user';
+import Profile from './Profile/Profile';
+
+const App = () => (
+  <Profile
+    username={user.username}
+    tag={user.tag}
+    location={user.location}
+    avatar={user.avatar}
+    stats={user.stats}
+  />
+);
 
 export default App;
