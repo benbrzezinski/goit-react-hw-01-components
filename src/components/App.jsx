d01cdf3ea2ac816887d1408e1fa917a3311465e0
Beniamin Brzeziński <benek9311@wp.pl>
2023-05-09 16:57:10 +0200

    Added statistics component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,20 @@
 import user from '../json/user';
 import Profile from './Profile/Profile';
+import data from '../json/data';
+import Statistics from './Statistics/Statistics';
 
 const App = () => (
-  <Profile
-    username={user.username}
-    tag={user.tag}
-    location={user.location}
-    avatar={user.avatar}
-    stats={user.stats}
-  />
+  <>
+    <Profile
+      username={user.username}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats}
+    />
+    <Statistics title="Upload stats" stats={data} />
+    <Statistics stats={data} />
+  </>
 );
 
 export default App;
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,28 @@
+import PropTypes from 'prop-types';
+import css from './Statistics.module.css';
+import getRandomHexColor from 'utils/getRandomHexColor';
+
+const Statistics = ({ title, stats }) => (
+  <section className={css.statistics}>
+    {title && <h2 className={css.title}>{title}</h2>}
+    <ul className={css.statList}>
+      {stats.map(({ id, label, percentage }) => (
+        <li
+          className={css.item}
+          key={id}
+          style={{ backgroundColor: getRandomHexColor() }}
+        >
+          <span className={css.label}>{label}</span>
+          <span className={css.percentage}>{percentage}%</span>
+        </li>
+      ))}
+    </ul>
+  </section>
+);
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(PropTypes.object.isRequired),
+};
+
+export default Statistics;
